3d6d2f06ad965af15a8267b71d140d7f8039fdc9
Tim Herbst <therbst@worcester.edu>
2019-11-11 17:17:17 -0500

    Initial commit

---
+++ src/main/java/simulated/os/rest/api/Test.java
@@ -0,0 +1,10 @@
+package main.java.simulated.os.rest.api;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("this is a test");
+
+	}
+
+}
---
+++ src/test/java/simulated/os/rest/api/Tester.java
@@ -0,0 +1,14 @@
+package test.java.simulated.os.rest.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Tester {
+
+	@Test
+	void test() {
+		assertEquals(1,1);
+	}
+
+}
