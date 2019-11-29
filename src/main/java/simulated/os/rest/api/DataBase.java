08765722209297b4261a2aded23a5c99679f8a03
Tim Herbst <therbst@worcester.edu>
2019-11-28 23:14:52 -0500

    implemented database

--- src/main/java/simulated/os/rest/api/DataBase.java
+++ src/main/java/simulated/os/rest/api/DataBase.java
@@ -1,5 +1,27 @@
 package main.java.simulated.os.rest.api;
 
-public class DataBase {
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class DataBase {
+	private static BlockTable blockTable = new BlockTable();
+	private static MemoryTable memoryTable = new MemoryTable();
+	private static JobTable jobTable = new JobTable();
+	private static AtomicInteger blockCounter = new AtomicInteger();
+	private static AtomicInteger jobCounter = new AtomicInteger();
+	
+	public static BlockTable getBlockTable() {
+		return blockTable;
+	}
+	public static MemoryTable getMemoryTable() {
+		return memoryTable;
+	}
+	public static JobTable getJobTable() {
+		return jobTable;
+	}
+	public static AtomicInteger getBlockCounter() {
+		return blockCounter;
+	}
+	public static AtomicInteger getJobCounter() {
+		return jobCounter;
+	}
 }
