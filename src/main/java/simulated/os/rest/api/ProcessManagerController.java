0978b736758373798c5e430106aac9ad0a1c0afb
Tim Herbst <therbst@worcester.edu>
2019-12-04 17:53:32 -0500

    added blank controllers

---
+++ src/main/java/simulated/os/rest/api/ProcessManagerController.java
@@ -0,0 +1,15 @@
+package main.java.simulated.os.rest.api;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+public class ProcessManagerController {
+
+}
